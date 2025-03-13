a81487704f03fb54a53b2120bd45ea80852b3a71
Theo <73302483+TheoSrss@users.noreply.github.com>
2025-03-13 17:36:06 +0100

    update for build

--- src/components/Dashboard/BtcPriceOriginal.tsx
+++ src/components/Dashboard/BtcPriceOriginal.tsx
@@ -1,72 +1,76 @@
-'use client';
+"use client";
 
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {Skeleton} from "@/components/ui/skeleton";
 
+// Définition des types
+type Commodity = {
+    key: string; name: string; today: number;
+} & {
+    [key: string]: string | number;
+};
+
 const BtcPriceOriginal = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<Commodity[]>([]);
     const [commodityKey, setCommodityKey] = useState<string>("WEGGS");
-    const [commodity, setCommodity] = useState<object>({});
+    const [commodity, setCommodity] = useState<Commodity | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = async () => {
+    // Fonction pour récupérer les données
+    const fetchData = useCallback(async () => {
         try {
-            const res = await fetch(`https://api.pricedinbitcoin21.com/api/bitcoin-stats-v2/summary?type=commodity&subType=agriculture`);
-            const json = await res.json();
-            if (json) {
+            const res = await fetch("https://api.pricedinbitcoin21.com/api/bitcoin-stats-v2/summary?type=commodity&subType=agriculture");
+            const json: Commodity[] = await res.json();
+            if (json.length > 0) {
                 setData(json);
-                updateCommodity()
-                console.log(json)
-                // let commodityFind = json.find((item: { key: string }) => item.key === commodityKey);
-                // console.log(commodityFind)
-                // setCommodity(commodityFind);
+                updateCommodity(json, commodityKey);
                 setLoading(false);
             }
-        } catch  {
+        } catch (error) {
+            console.error("Error fetching data:", error);
             setLoading(true);
+        }
+    }, [commodityKey]);
+
+    // Fonction pour mettre à jour la sélection du commodity
+    const updateCommodity = (commodities = data, key = commodityKey) => {
+        const selectedCommodity = commodities.find((item) => item.key === key);
+        if (selectedCommodity) {
+            setCommodity(selectedCommodity);
+            setCommodityKey(selectedCommodity.key);
         }
     };
 
-    const updateCommodity = (key=commodityKey) => {
-        const commodityFind = data.find((item: { key: string }) => item.key === key);
-        setCommodity(commodityFind);
-        setCommodityKey(commodityFind.key);
-    }
+    // Récupération des données au montage
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
 
-
-    console.log(commodity)
     return (<Card className="w-full max-w-md mx-auto">
-        {/*{(commodity.today*100000000).toFixed(0)}*/}
-        <CardHeader className="flex flex-col  justify-between gap-2">
-            <CardTitle>Commodities Price in Sats</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col-reverse sm:flex-row items-center justify-between gap-6 sm:p-4">
-            <div className="text-2xl font-bold flex flex-col gap-2">
-
-                <span className="whitespace-nowrap">
-                    {!loading ? `1 ${commodity.name} = ${(commodity.today * 100000000).toFixed()} Satoshi` : <Skeleton className="h-8 w-full"/>}
-                    {/*PRICE*/}
-          </span>
-            </div>
-            {!loading ? (<Select onValueChange={(e) => updateCommodity(e) } value={commodityKey}>
-                    <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder='commodityKey'/>
-                    </SelectTrigger>
-                    <SelectContent>
-                        {data.map((item: { key: string }) => (
-                            <SelectItem key={item.key} value={item.key}>{item.name}</SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>) :<Skeleton className="h-8 w-20"/>
-            }
-
-        </CardContent>
-    </Card>);
+            <CardHeader className="flex flex-col justify-between gap-2">
+                <CardTitle>Commodities Price in Sats</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col-reverse sm:flex-row items-center justify-between gap-6 sm:p-4">
+                <div className="text-2xl font-bold flex flex-col gap-2">
+                    <span className="whitespace-nowrap">
+                        {!loading && commodity ? (`1 ${commodity.name} = ${(commodity.today * 100000000).toFixed(0)} Satoshi`) : (
+                            <Skeleton className="h-8 w-full"/>)}
+                    </span>
+                </div>
+                {!loading ? (<Select onValueChange={(e) => updateCommodity(data, e)} value={commodityKey}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Select Commodity"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            {data.map((item) => (<SelectItem key={item.key} value={item.key}>
+                                    {item.name}
+                                </SelectItem>))}
+                        </SelectContent>
+                    </Select>) : (<Skeleton className="h-8 w-20"/>)}
+            </CardContent>
+        </Card>);
 };
+
 export default BtcPriceOriginal;
