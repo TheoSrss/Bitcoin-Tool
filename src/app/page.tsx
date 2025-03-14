4ff2f6401c22abbc6794d09cc00a0e9a3ddd5a13
Theo <73302483+TheoSrss@users.noreply.github.com>
2025-03-14 19:08:40 +0100

    clean dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import BtcPrice from "@/components/Dashboard/BtcPrice";
 import BtcChart from "@/components/Dashboard/BtcChart";
-import BlocksInformations from "@/components/Dashboard/BlocksInformations";
-import BtcPriceOriginal from "@/components/Dashboard/BtcPriceOriginal";
 
 export default function Dashboard() {
     return (
         <div className='mt-10'>
             <div className='flex flex-row '>
                 <BtcPrice/>
-                <BtcPriceOriginal/>
-                <BlocksInformations/>
             </div>
             <BtcChart/>
         </div>
